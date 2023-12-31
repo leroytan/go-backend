ef5c2ce3ab75e48f5dbee0f259027a11644098b6
Leroy Tan <mingxiang130@gmail.com>
2024-01-01 03:43:20 +0800

    Updated CORS

--- main.go
+++ main.go
@@ -10,14 +10,29 @@ func init() {
 	initializers.ConnectToDB()
 	initializers.LoadEnvVariables()
 }
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 func main() {
 
 	r := gin.Default()
-	r.POST("/posts", controllers.PostsCreate)
-	r.PUT("/posts/:id", controllers.PostsUpdate)
-	r.GET("/posts", controllers.PostsIndex)
-	r.GET("/posts/:id", controllers.PostsShow)
-	r.DELETE("/posts/:id", controllers.PostsSoftDelete)
-	r.Run() // listen and serve on 0.0.0.0:3000
+	r.Use(CORSMiddleware())
+	r.POST("/api/posts", controllers.PostsCreate)
+	r.PUT("/api/posts/:id", controllers.PostsUpdate)
+	r.GET("/api/posts", controllers.PostsIndex)
+	r.GET("/api/posts/:id", controllers.PostsShow)
+	r.DELETE("/api/posts/:id", controllers.PostsSoftDelete)
+	r.Run(":3000") // listen and serve on 0.0.0.0:3000
 }
